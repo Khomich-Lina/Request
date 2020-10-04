85fcb8769627ae1f44d78943b9df4477ba8dc404
Khomich-Lina <49281281+Khomich-Lina@users.noreply.github.com>
2020-10-04 13:19:13 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const base_url = 'http://142.93.134.108:1111'
+
+
+const form = {
+    username: document.getElementById('username'),
+    password: document.getElementById('password'),
+    submit: document.getElementById('sign_up'),
+    submit2: document.getElementById('login')
+};
+
+form.submit.addEventListener('click', () => {
+
+    let email = form.username.value;
+    let password = form.password.value;
+
+    axios({
+        method: 'post',
+        url: base_url + '/sign_up',
+        data: {
+            email: email,
+            password: password
+        }
+    });});
+
+form.submit2.addEventListener('click', () => {
+
+    let email = form.username.value;
+    let password = form.password.value;
+
+    axios({
+        method: 'post',
+        url: base_url + '/login?' + `email=${email}&password=${password}`,
+    }).then(response=> console.log(response))
+    check_token();
+});
+
+function check_token(access, refresh){
+    let access_token = access;
+    let refresh_token = refresh;
+    axios({
+        method: 'get',
+        url: base_url + `/me`,
+        headers:{Authorization: `Bearer ${access_token}`}})
+        .then(response=>response.status===401?function (result){update_token(result.data.body.message, refresh_token)}:null)
+}
+
+function update_token(data, refresh_token){
+    let message = data;
+    if(message =="token expired"){
+        axios({
+            method: 'post',
+            url: base_url + `/refresh`,
+            headers:{Authorization: `Bearer ${refresh_token}`}})
+            .then(function (result){check_token(result.data.body.access_token)})
+    }
+}
